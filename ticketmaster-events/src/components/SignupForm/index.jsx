308cbb0e5ec0d00f494659a6585a7a82ceb221ef
Ruben Roinich <roinichruben@gmail.com>
2024-02-22 07:55:06 -0300

    Migrando a react form hook parte 2

--- ticketmaster-events/src/components/SignupForm/index.jsx
+++ ticketmaster-events/src/components/SignupForm/index.jsx
@@ -1,60 +1,44 @@
-import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 const SignupForm = () => {
-    const { register, handleSubmit } = useForm('')
-
-   /*  const [name, setName] = useState('');
-    const [age, setAge] = useState('');
-    const [address, setAdress] = useState('');
-    const [zipcode, setZipcode] = useState('');
-    const [phone, setPhone] = useState(''); Queda comentado por la implementación del react-hook-form*/
+    const { register, handleSubmit, reset, formState:{errors} } = useForm('')
 
     const handleClearClick =() =>{
-      /*   setName('');
-        setAge('');
-        setAdress('');
-        setZipcode('');
-        setPhone(''); */
-
+        reset();
+      
     };
 
     const handleSubmitForm =(data) =>{
-      console.log(data);
-    /*     console.log('submit:',{
-            name,
-            age,
-            address,
-            zipcode,
-            phone,
-        });*/
+     console.log(data);
+    
     }; 
+    console.log(errors);
 
    return (
         <form onSubmit={handleSubmit(handleSubmitForm)}>
             <label>
                 Name
-                <input {...register('name')} required />
+                <input {...register('name', {required: true})} />
             </label>
             <br />
             <label>
                 Age
-                <input {...register('age')}required  />
+                <input {...register('age', {required: true})} />
             </label>
             <br />
             <label>
                 Address
-                <input{...register('address')} required />
+                <input {...register('address', {required: true})} />
             </label>
             <br />
             <label>
                 Zipcode
-                <input {...register('zipcode')} required />
+                <input {...register('zipcode', {required: true})} />
             </label>
             <br />
             <label>
                 Phone
-                <input {...register('phone')} required />
+                <input {...register('phone', {required: true})} />
             </label>
             <br />
             <div>
